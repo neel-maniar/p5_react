5c597ca2921b9db1872f51f9ffe908ab802aec89
Neel Maniar <neelmaniar02@gmail.com>
2026-02-09 23:04:25 +0000

    DvD in the corner

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
 import React, { useCallback } from "react";
 import type p5 from "p5";
 import P5Background from "./P5Background";
+import Vector2 from "./Vector2";
+
+interface Particle {
+  pos: Vector2;
+  vel: Vector2;
+}
 
 interface SketchState {
-  x: number;
-  speed: number;
+  particles: Particle[];
 }
 
 const App: React.FC = () => {
-  const state: SketchState = { x: 0, speed: 0 };
+  const state: SketchState = {particles:[{pos: new Vector2(0, 0), vel: new Vector2(1,1)}]};
 
   const setup = useCallback((p: p5) => {
-    state.x = p.width / 2;
-    state.speed = p.width * 0.005;
+    state.particles[0].pos.x = p.width / 2;
+    state.particles[0].pos.y = p.height / 2;
+    state.particles[0].vel.x = p.width * 0.0005;
+    state.particles[0].vel.y = p.height * 0.0005;
   }, []);
+
 
   const draw = useCallback((p: p5) => {
     const radius = Math.min(p.width, p.height) * 0.05;
 
-    state.x += state.speed;
-    if (state.x + radius > p.width || state.x - radius < 0) {
-      state.speed = -state.speed;
-      state.x = Math.max(radius, Math.min(state.x, p.width - radius));
-    }
 
     p.background(255, 120, 20);
     p.noStroke();
     p.fill(255);
-    p.ellipse(state.x, p.height / 2, radius * 2);
+
+    for (let i = 0; i < state.particles.length; i++) {
+      state.particles[i].pos = Vector2.add(state.particles[i].pos, state.particles[i].vel);
+
+      if (state.particles[i].pos.x > p.width && state.particles[i].vel.x > 0) {
+        state.particles[i].vel.x = -state.particles[i].vel.x
+      }
+
+      if (state.particles[i].pos.x < 0 && state.particles[i].vel.x < 0) {
+        state.particles[i].vel.x = -state.particles[i].vel.x
+      }
+
+      if (state.particles[i].pos.y > p.height && state.particles[i].vel.y > 0) {
+        state.particles[i].vel.y = -state.particles[i].vel.y
+      }
+
+      if (state.particles[i].pos.y < 0 && state.particles[i].vel.y < 0) {
+        state.particles[i].vel.y = -state.particles[i].vel.y
+      }
+      p.ellipse(state.particles[i].pos.x, state.particles[i].pos.y, radius * 2);
+    }
   }, []);
 
   return (
---
+++ src/Vector2.ts
@@ -0,0 +1,56 @@
+export default class Vector2 {
+    public static add(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    }
+
+    public static mul(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x * b.x, a.y * b.y);
+    }
+
+    public static dot(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x * b.x + a.y * b.y);
+    }
+
+    public x: number;
+    public y: number;
+
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public get magnitude(): number {
+        return Math.sqrt(this.sqrMagnitude);
+    }
+
+    public get sqrMagnitude(): number {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    public normalize() {
+        const len = this.magnitude;
+        if (len > 0) {
+            this.x /= len;
+            this.y /= len;
+        }
+    }
+
+    public inverse() {
+        this.x = -this.x;
+        this.y = -this.y;
+    }
+
+    public get normalized(): Vector2 {
+        const v = new Vector2(this.x, this.y);
+        const len = this.magnitude;
+        if (len > 0) {
+            v.x /= len;
+            v.y /= len;
+        }
+        return v;
+    }
+
+    public get inversed(): Vector2 {
+        return new Vector2(-this.x, -this.y);
+    }
+}
